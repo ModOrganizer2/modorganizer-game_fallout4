fa4c0da82452dffa0ba6e2e54f848bb06643b3a9
Mikaël Capelle <capelle.mikael@gmail.com>
2021-02-02 20:39:12 +0100

    Update translation file.

--- src/game_fallout4_en.ts
+++ src/game_fallout4_en.ts
@@ -4,7 +4,12 @@
 <context>
     <name>GameFallout4</name>
     <message>
-        <location filename="gamefallout4.cpp" line="85"/>
+        <location filename="gamefallout4.cpp" line="81"/>
+        <source>Fallout 4 Support Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamefallout4.cpp" line="92"/>
         <source>Adds support for the game Fallout 4.
 Splash by %1</source>
         <translation type="unfinished"></translation>
